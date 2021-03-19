e5dfed19f34df392cec720c489d0b47d306110b9
Pawel Galazka <829242+pawelgalazka@users.noreply.github.com>
2021-03-19 23:37:42 +1100

    Add notifier enhancements

--- index.js
+++ index.js
@@ -6,23 +6,33 @@ function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function log(msg) {
+  console.log(`[${new Date().toLocaleString()}] ${msg}`);
+}
+
+const WAIT_VALUE_IN_SEC = 30;
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   while (true) {
     for (const { url, test } of config) {
+      log(`Checking ${url}`);
       await page.goto(url);
       const message = await page.evaluate(test);
       if (message) {
         notifier.notify({
           title: "Web Lookout",
           message,
+          sticky: true,
+          open: url,
         });
       }
     }
 
-    await timeout(30000);
+    log(`Waiting for ${WAIT_VALUE_IN_SEC} sec`);
+    await timeout(WAIT_VALUE_IN_SEC * 100);
   }
 
   await browser.close();
